fb196ebf4ab9e397af0d0385dbeb39efeb5ea7d1
JRowley <J.Rowley@UCL.Economics>
2012-07-20 10:01:03 +0100

    Finished code for asymptotic distribution.
    Constructs a grid of points and tests whether these points lie in the
    identified set. Output is a rather crude set of points on a plot.

--- Executable.R
+++ Executable.R
@@ -2,8 +2,8 @@ library("mvtnorm")
 
 # Enter parameters of the model.
 
-M <- 6
-C <- c(-Inf,-0.3,-0.1,0,0.1,0.3,Inf)
+M <- 4
+C <- c(-Inf,-0.1,0,0.1,Inf)
 
 X <- c(-1,1)
 G <- c(-Inf,0,Inf)
@@ -18,52 +18,50 @@ Sigma <- matrix(c(1,0.5,0.5,1),nrow=2,ncol=2)
 
 # Enter grid of values over which you wish to search for the identified set.
 
-a_0 <- seq(0,2,0.5)
-a_1 <- seq(0,2,0.5)
+a_0 <- seq(-0.5 , 0.5 , length.out = 25)
+a_1 <- seq(0.3 , 1.6 , length.out = 25)
 
 # The following code will create a data.frame of all possible combinations of (a_0 , a_1 , Y , X , Z). 
 
 Y <- seq(1 , M , 1)
-frame <- expand.grid(a_0 , a_1 , Y , X , Z)
-colnames(frame) <- c("a_0" , "a_1" , "Y" , "X" , "Z")
+Pr.frame <- expand.grid(Y , X , Z , a_0 , a_1 )
+colnames(Pr.frame) <- c("Y" , "X" , "Z" , "a_0" , "a_1")
 
 # We now need to calculate the probability of the event (Y=m and X=x | Z=z) using the mvtnorm package. 
 
 Pr <- NULL
-for(i in seq(1,nrow(frame),1)){
-  Pr <- c(Pr , pmvnorm(lower = c(C[match(frame$Y[i] , Y)] - alpha_1*frame$X[i] , G[match(frame$X[i] , X)] - 0.5*frame$Z[i]) , upper = c(C[match(frame$Y[i] , Y) + 1] - alpha_1*frame$X[i] , G[match(frame$X[i] , X) + 1] - 0.5*frame$Z[i]) , mean = Mu , sigma = Sigma)) 
+for(i in seq(1,nrow(Pr.frame),1)){
+  Pr <- c(Pr , pmvnorm(lower = c(C[match(Pr.frame$Y[i] , Y)] - alpha_1*Pr.frame$X[i] , G[match(Pr.frame$X[i] , X)] - 0.5*Pr.frame$Z[i]) , upper = c(C[match(Pr.frame$Y[i] , Y) + 1] - alpha_1*Pr.frame$X[i] , G[match(Pr.frame$X[i] , X) + 1] - 0.5*Pr.frame$Z[i]) , mean = Mu , sigma = Sigma)) 
 }
 
 # Combine these probabilities with the data.frame.
 
-Pr.frame <- data.frame(cbind(frame , Pr))
-colnames(Pr.frame) <- c("a_0" , "a_1" , "Y" , "X" , "Z" , "Pr")
+Pr.frame$Pr <- Pr
 
 # Cumulate these probabilities for Y<=m.
 
 cum.Pr <- NULL
-for(i in seq(1,nrow(frame),1)){
+for(i in seq(1,nrow(Pr.frame),1)){
   cum.Pr <- c(cum.Pr , with(Pr.frame , sum(Pr.frame[Pr.frame$Z[i] == Pr.frame$Z & Pr.frame$X[i] == Pr.frame$X & Pr.frame$Y[i] >= Pr.frame$Y & Pr.frame$a_1[i] == Pr.frame$a_1 & Pr.frame$a_0[i] == Pr.frame$a_0 , "Pr"]))) 
 }
 
 # Combine these probabilities with the data.frame.
 
-frame <- data.frame(cbind(Pr.frame , cum.Pr))
-colnames(frame) <- c("a_0" , "a_1" , "Y" , "X" , "Z" , "Pr" , "cum.Pr")
+Pr.frame$cum.Pr <- cum.Pr
 
 # Compute parameter values.
 
 Par_L <- NULL
 Par_U <- NULL
-for(i in seq(1,nrow(frame),1)){
-  Par_L <- c(Par_L , C[match(frame$Y[i] , Y)] - frame$a_0[i] - frame$a_1[i]*frame$X[i])
-  Par_U <- c(Par_U , C[match(frame$Y[i] , Y) + 1] - frame$a_0[i] - frame$a_1[i]*frame$X[i])
+for(i in seq(1,nrow(Pr.frame),1)){
+  Par_L <- c(Par_L , C[match(Pr.frame$Y[i] , Y)] - Pr.frame$a_0[i] - Pr.frame$a_1[i]*Pr.frame$X[i])
+  Par_U <- c(Par_U , C[match(Pr.frame$Y[i] , Y) + 1] - Pr.frame$a_0[i] - Pr.frame$a_1[i]*Pr.frame$X[i])
 }
 
 # Combine these parameters with the data.frame.
 
-Pr.frame <- data.frame(cbind(frame , Par_L , Par_U))
-colnames(Pr.frame) <- c("a_0" , "a_1" , "Y" , "X" , "Z" , "Pr" , "cum.Pr" , "Par_L" , "Par_U")
+Pr.frame$Par_L <- Par_L
+Pr.frame$Par_U <- Par_U
 
 # Normalise parameters such that they fall in the [0,1] interval.
 
@@ -73,17 +71,65 @@ Pr.frame <- transform(Pr.frame , Par_U=pnorm(Par_U))
 # The first condition for a combination (a_0 , a_1) lying inside the identified set to be tested.
 
 output.1 <- NULL
-for(i in seq(1,nrow(frame),1)){
-  output.1 <- c(output.1 , with(Pr.frame , sum(Pr.frame[Pr.frame$Par_L[i] >= Pr.frame$Par_L & Pr.frame$Z[i] == Pr.frame$Z & Pr.frame$a_0[i] == Pr.frame$a_0 & Pr.frame$a_1[i] == Pr.frame$a_1 & Pr.frame$Y != M, "Pr"])))
+for(i in seq(1,nrow(Pr.frame),1)){
+  output.1 <- c(output.1 , with(Pr.frame , sum(Pr.frame[Pr.frame$Par_U[i] >= Pr.frame$Par_U & Pr.frame$Z[i] == Pr.frame$Z & Pr.frame$a_0[i] == Pr.frame$a_0 & Pr.frame$a_1[i] == Pr.frame$a_1 & Pr.frame$Y != M, "Pr"])))
 } 
-condition.1 <- ifelse(output.1 <= Pr.frame$Par_L , 1 , 0)
+condition.1 <- ifelse(output.1 <= Pr.frame$Par_U , TRUE , FALSE)
 
 # The second condition for a combination (a_0 , a_1) lying inside the identified set to be tested.
 
 output.2 <- NULL
-for(i in seq(1,nrow(frame),1)){
-  output.2 <- c(output.1 , with(Pr.frame , sum(Pr.frame[Pr.frame$Par_L[i] > Pr.frame$Par_L & Pr.frame$Z[i] == Pr.frame$Z & Pr.frame$a_0[i] == Pr.frame$a_0 & Pr.frame$a_1[i] == Pr.frame$a_1 , "Pr"])))
+for(i in seq(1,nrow(Pr.frame),1)){
+  output.2 <- c(output.2 , with(Pr.frame , sum(Pr.frame[Pr.frame$Par_U[i] > Pr.frame$Par_L & Pr.frame$Z[i] == Pr.frame$Z & Pr.frame$a_0[i] == Pr.frame$a_0 & Pr.frame$a_1[i] == Pr.frame$a_1 , "Pr"])))
 } 
-condition.2 <- ifelse(output.1 >= Pr.frame$Par_L , 1 , 0)
+condition.2 <- ifelse(output.2 >= Pr.frame$Par_U | Pr.frame$Y == M , TRUE , FALSE)
 
 # The third condition for a combination (a_0 , a_1) lying inside the identified set to be tested.
+
+output.3 <- NULL
+for(i in seq(1,nrow(Pr.frame),1)){
+  output.3 <- c(output.3 , ifelse(Pr.frame$Par_U[i] - Pr.frame$cum.Pr[i] >= 0, TRUE , FALSE))
+}
+
+output.4 <- NULL
+for(i in seq(1,nrow(Pr.frame),1)){
+  output.4 <- c(output.4 , ifelse(any(ifelse(
+    (Pr.frame$Par_U[i] - min(Pr.frame[Pr.frame$Y[i] > Pr.frame$Y & Pr.frame$X[i] == Pr.frame$X & Pr.frame$Z[i] == Pr.frame$Z & Pr.frame$a_0[i] == Pr.frame$a_0 & Pr.frame$a_1[i] == Pr.frame$a_1 , "Par_U"] , 0)) - 
+      (Pr.frame$cum.Pr[i] - min(Pr.frame[Pr.frame$Y[i] > Pr.frame$Y & Pr.frame$X[i] == Pr.frame$X & Pr.frame$Z[i] == Pr.frame$Z & Pr.frame$a_0[i] == Pr.frame$a_0 & Pr.frame$a_1[i] == Pr.frame$a_1 , "cum.Pr"] , 0)) >= 0 | Pr.frame$Y[i] == 1 , 
+    TRUE , FALSE)) ,
+                                  TRUE , FALSE))
+}
+
+condition.3 <- as.logical(output.3 * output.4)
+
+# Combine conditions into one identification condition.
+
+Identification <- as.logical(condition.1 * condition.2 * condition.3)
+
+# Attach Identification results to the data.frame.
+
+Pr.frame$Identification <- Identification
+
+# We now need to combine the identification conditions for each individual parameter into a vector that can be checked.
+
+Results <- expand.grid(a_0 , a_1)
+colnames(Results) <- c("a_0" , "a_1")
+output.final <- NULL
+for(i in seq(1,nrow(Results),1)){
+  output.final <- c(output.final , ifelse(
+    all(
+      Pr.frame[Pr.frame$a_0 == Results$a_0[i] & Pr.frame$a_1 == Results$a_1[i] , "Identification"]) == TRUE ,
+    TRUE , FALSE))
+}
+
+# Final results can be compiled in the following data.frame.
+
+Results$Identified <- output.final
+
+# Restrict attention to the identified set.
+
+library(ggplot2)
+
+Graphical <- subset(Results , Identified == TRUE , select = c(a_0 , a_1))
+p <- ggplot(data = Graphical , aes(x = a_0 , y = a_1 ))
+p + geom_point(alpha = 0.3)
